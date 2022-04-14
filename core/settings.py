0f3fe115102e554011f2f7bc48011c7a1ab19701
trigg8 <tannerrigg@gmail.com>
2022-04-14 16:35:12 +0700

    refractoring finished

--- core/settings.py
+++ core/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+from django.urls import reverse
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -52,7 +53,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'store.views.categories',
+                'store.context_processors.categories',
             ],
         },
     },
@@ -107,6 +108,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
--- store/views.py
+++ store/views.py
@@ -2,16 +2,15 @@ from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 
 
-def categories(request):
-    return {"categories": Category.objects.all()}
-
-def all_products(request):
-    products = Product.objects.all()
+def product_all(request):
+    products = Product.products.all()
     return render(request, 'store/home.html', {'products': products })
+
 
 def product_detail(request, slug):
     product = get_object_or_404(Product, slug=slug, in_stock=True)
-    return render(request, 'store/products/detail.html', {'product': product})
+    return render(request, 'store/products/single.html', {'product': product})
+
 
 def category_list(request, category_slug):
     category = get_object_or_404(Category, slug=category_slug)
