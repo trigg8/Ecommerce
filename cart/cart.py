3d765e16dff5ce37882995b68515c587eef5f089
trigg8 <tannerrigg@gmail.com>
2022-04-15 12:44:02 +0700

    added session cart and adding to the cart

--- cart/cart.py
+++ cart/cart.py
@@ -1,4 +1,4 @@
-
+import simplejson as json
 
 
 class Cart():
@@ -11,3 +11,20 @@ class Cart():
         if 'skey' not in request.session:
             cart = self.session['skey'] = {}
         self.cart = cart
+
+    def add(self, product, qty):
+        '''
+        Adding and updating the users cart session data
+        '''
+        product_id = product.id
+
+        if product_id not in self.cart:
+            self.cart[product_id] = {'price': str(product.price), 'qty':int(qty)}
+
+        self.session.modified = True
+
+    def __len__(self):
+        '''
+        Get the cart data and count the qty of items
+        '''
+        return sum(item['qty'] for item in self.cart.values())
--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404, render
+from django.http import JsonResponse
+from .cart import Cart
+from store.models import Product
 
 def cart_summary(request):
     return render(request, 'store/cart/summary.html')
+
+
+def cart_add(request):
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('productid'))
+        product_qty = int(request.POST.get('productqty'))
+        product = get_object_or_404(Product, id=product_id)
+        cart.add(product=product, qty=product_qty)
+        cartqty = cart.__len__()
+        response = JsonResponse({'qty': cartqty})
+        return response
