aca5e643040ada8f4410312058064253aabd2337
trigg8 <tannerrigg@gmail.com>
2022-04-17 13:35:29 +0700

    User management completed

--- account/forms.py
+++ account/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from .models import UserBase
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, PasswordResetForm, SetPasswordForm
 
 
 class UserLoginForm(AuthenticationForm):
@@ -61,3 +61,22 @@ class UserEditForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         self.fields['user_name'].required= True
         self.fields['email'].required = True
+
+
+class PwdResetForm(PasswordResetForm):
+
+    email = forms.EmailField(max_length=254, widget=forms.TextInput(attrs={'class': 'form-control mb-3', 'placeholder': 'Email', 'id': 'form-email'}))
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        u = UserBase.objects.filter(email=email)
+        if not u:
+            raise forms.ValidationError('Unfortunatley we can not find that email address.')
+        return email
+
+
+class PwdResetConfirmForm(SetPasswordForm):
+
+    new_password1 = forms.CharField(label= 'New Password', widget=forms.PasswordInput(attrs={'class': 'form-control mb-3', 'placeholder': 'New Password', 'id': 'form-newpass'}))
+
+    new_password2 = forms.CharField(label='Repeat Password', widget=forms.PasswordInput(attrs={'class': 'form-control mb-3', 'placeholder': 'New Password', 'id': 'form-new-pass2'}))
